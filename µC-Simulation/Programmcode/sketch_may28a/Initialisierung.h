1eddcbec336233c79ea379016d87e790c57adba2
Martin Maucher <ma96.maucher@online.de>
2020-06-04 19:32:44 +0200

    changed the program

---
+++ µC-Simulation/Programmcode/sketch_may28a/Initialisierung.h
@@ -0,0 +1,96 @@
+#include <Arduino.h>
+#include <Wire.h> // I2C Bibleothek
+#include <LiquidCrystal_I2C.h>  //Bibleothek für I2C Displayansteuerung
+#include <LowPower.h> // Bibleothek für Low Power Mode
+#include <avr/wdt.h>  //Bibleothek für Watchdogtimer
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // LCD Adresse ist 0x27, 16 Zeichen und 2 Zeilen
+
+//
+//Variablen Deklaration
+//
+int InterruptPin = PB2; //Externer Interrupt an Pin PB2 wird verwendet
+int val[8], volt[8], pin=32, Zustand=0,i=0;
+bool Starttaster=false, Fehler=false;
+
+void InitializeTimers()
+{
+  wdt_disable();  // Watchdog ausschalten
+  
+  //
+  // Ausgabepins initialisieren
+  //
+  pinMode(PD5,OUTPUT);
+  pinMode(PB6,OUTPUT);
+  digitalWrite(PD5,LOW);
+  digitalWrite(PB6,LOW);
+
+  //
+  // Timer 1 (16bit) für PWM mit variabler Frequenz initialisieren
+  //
+  TCCR1A = 0b10000010;  //  OC1A löschen, bei Compare Match (Fast PWM Mode 14, mit Top = ICR1)
+  TCNT1=0;  // Timer 1 zurücksetzen
+  ICR1 = 100;  // Zählerstand für Periodendauer (79,21kHz)
+  OCR1A = 70; // Compare Wert für Impulsdauer (70%)
+
+  //
+  // Timer 3 (16bit)  für PWM mit variaabler Frequenz initialisieren
+  //
+  TCCR3A = 0b10000010;  //  OC3A löschen, bei Compare Match (Fast PWM Mode 14, mit Top = ICR3)
+  TCNT3=0;  // Timer 3 zurücksetzen
+  ICR3 = 100;  // Zählerstand für Periodendauer (79,21kHz)
+  OCR3A = 70; // Compare Wert für Impulsdauer (70%)
+}
+
+void InitializeDisplay()
+{
+  Serial.begin(9600); //Baudrate der seriellen Schnittstelle wird auf 9600 gesetzt
+  //
+  // Initialisierung des Displays
+  //
+  lcd.init(); // LCD Display wird initialisiert 
+  lcd.backlight();  // Hintergrundbeleuchtung wird angeschalten
+  lcd.setCursor(0,0); // Coursor wird auf erstes Zeichen in der ersetn Zeile gesetzt
+  lcd.print("Solar Laderegler"); // Auf dem Display wird "Hello, world!" ausgegeben
+  lcd.setCursor(0,1);
+  lcd.print("Gruppe 2");
+  delay(1000);
+}
+
+void InitializePorts()
+{
+  //
+  // Analogeingänge konfigurieren
+  //
+  for(int i=0;i<8;i++)
+  {
+    pinMode(pin,INPUT);
+    pin++;
+  }
+  pin=32;
+}
+
+void TimerStart()
+{
+  //
+  //  Timer starten
+  //
+  TCCR1B = 0b00011001; // Vorteiler = 1
+  delayMicroseconds(58);  // 180° Phasenverschiebung
+  TCCR3B = 0b00011001; // Vorteiler = 1
+}
+
+void TimerStop()
+{
+  //
+  //  Timer stoppen
+  //
+  TCCR1B = 0b00011000;
+  TCCR3B = 0b00011000;
+  TCNT1=0;
+  TCNT3=0;
+  delay(200);
+  digitalWrite(PD5,LOW);
+  digitalWrite(PB6,LOW);
+}
+
--- µC-Simulation/Programmcode/sketch_may28a/sketch_may28a.ino
+++ µC-Simulation/Programmcode/sketch_may28a/sketch_may28a.ino
@@ -1,106 +1,128 @@
-#include <LowPower.h> // Bibleothek für Low Power Mode
-#include <Wire.h> // I2C Bibleothek
-#include <LiquidCrystal_I2C.h>  //Bibleothek für I2C Displayansteuerung
-#include <avr/wdt.h>
-
-LiquidCrystal_I2C lcd(0x27,20,4);  // LCD Adresse ist 0x27, 16 Zeichen und 2 Zeilen
-
-//
-//Variablen Deklaration
-//
-int InterruptPin = PB2; //Externer Interrupt an Pin PB2 wird verwendet
-int val[8], volt[8], pin=32;
+#include "Initialisierung.h"
 
 void setup()
 {
-  wdt_disable();
-  Serial.begin(9600); //Baudrate der seriellen Schnittstelle wird auf 9600 gesetzt
   
-  //
-  // Externer Interrupt wird konfiguriert: Interruptname (INT2) für PB2 wird gesucht, die ISR ist 
-  // WakeUpHandler und es wird auf eine fallende Flanke reagiert
-  //
-  attachInterrupt(digitalPinToInterrupt(InterruptPin), WakeUpHandler, FALLING); 
-
-  //
-  // Initialisierung des Displays
-  //
-  lcd.init(); // LCD Display wird initialisiert 
-  lcd.backlight();  // Hintergrundbeleuchtung wird angeschalten
-  lcd.setCursor(1,0); // Coursor wird auf erstes Zeichen in der ersetn Zeile gesetzt
-  lcd.print("Hello, world!"); // Auf dem Display wird "Hello, world!" ausgegeben
-
-  //
-  // Analogeingänge konfigurieren
-  //
-  for(int i=0;i<8;i++)
-  {
-    pinMode(pin,INPUT);
-    pin++;
-  }
-  pin=32;
-
-  pinMode(PD5,OUTPUT);
-  pinMode(PB6,OUTPUT);
-  digitalWrite(PD5,LOW);
-  digitalWrite(PB6,LOW);
-
-  
-  //
-  // Timer 1 (16bit) für PWM mit variabler Frequenz initialisieren
-  //
-  TCCR1A = 0b10000010;  //  OC1A löschen, bei Compare Match (Fast PWM Mode 14, mit Top = ICR1)
-  TCNT1=0;  // Timer 1 zurücksetzen
-  ICR1 = 100;  // Zählerstand für Periodendauer (79,21kHz)
-  OCR1A = 70; // Compare Wert für Impulsdauer (70%)
-
-  //
-  // Timer 3 (16bit)  für PWM mit variaabler Frequenz initialisieren
-  //
-  TCCR3A = 0b10000010;  //  OC3A löschen, bei Compare Match (Fast PWM Mode 14, mit Top = ICR3)
-  TCNT3=0;  // Timer 3 zurücksetzen
-  ICR3 = 100;  // Zählerstand für Periodendauer (79,21kHz)
-  OCR3A = 70; // Compare Wert für Impulsdauer (70%)
-  
-  TCCR1B = 0b00011001; // Vorteiler = 1
-  delayMicroseconds(58);  // 180° Phasenverschiebung
-  TCCR3B = 0b00011001; // Vorteiler = 1
 }
-
 
 void loop()
 {
-  Serial.println("Das ist meine Debugschnittstelle"); // Auf dem Terminal wird "Das ist meine Debugschnittstelle" ausgegeben
-  delay(500); // 500ms Wartezeit
-  
-  //
-  // Analogeingäng einlesen
-  //
-  for(int i=0;i<8;i++)
+  switch(Zustand)
   {
-    val[i]=analogRead(pin);
-    volt[i]=map(val[i],0,1023,0,500);
-    Serial.println(volt[i]/100.0);
-    delay(300);
-    pin++;
+    case 0: // Initialisierung
+    {
+      //
+      // Externer Interrupt wird konfiguriert: Interruptname (INT2) für PB2 wird gesucht, die ISR ist 
+      // WakeUpHandler und es wird auf eine fallende Flanke reagiert
+      //
+      attachInterrupt(digitalPinToInterrupt(InterruptPin), WakeUpHandler, FALLING);
+      InitializeDisplay();
+      InitializePorts();
+      InitializeTimers();
+      if(Starttaster)
+      {
+        Zustand=2;
+      }
+      else
+      {
+        Zustand=1;
+      }
+    }
+    case 1: // Standby
+    {
+      i=0;
+      lcd.clear();
+      lcd.print("Standby");
+      delay(200); // Wartezeit, um den Text auszugeben, bevor das Display ausgeschaltet wird
+      lcd.noBacklight();  // Hintergrundbeleuchtung wird ausgeschalten
+      lcd.noDisplay();  // Textanzeige wird ausgeschalten
+      LowPower.powerDown(SLEEP_FOREVER,ADC_OFF,BOD_OFF); // Power Down Mode wird aktiviert und erst bei externem Interrupt oder Reset wieder ausgeschalten
+      Starttaster=false;
+      lcd.backlight();
+      lcd.display();
+      lcd.clear();  // Text in Display löschen
+      lcd.print("Aktiv");
+      delay(500);
+      Zustand=2;
+      TimerStart(); // PWM wird gestartet
+      break;
+    }
+    case 2: // Aktiv
+    {
+      //
+      // Analogeingäng einlesen
+      //
+      for(i;i<8;i++)
+      {
+        val[i]=analogRead(pin);
+        volt[i]=map(val[i],0,1023,0,500);
+        Serial.println(volt[i]/100.0);
+        if((volt[i]/100.0)<1)
+        {
+          Fehler=true;
+          break;
+        }
+        else
+        {
+          delay(300);
+          pin++;
+        }
+      }
+      if(Fehler)
+      {
+        Zustand=-1;
+      }
+      else
+      {
+        pin=32;
+        delay(500);
+        Zustand=1;
+        TimerStart();
+      }
+      break;
+    }
+    case -1:  //Fehler
+    {
+      //
+      // Externer Interrupt wird konfiguriert: Interruptname (INT2) für PB2 wird gesucht, die ISR wird von 
+      // WakeUpHandler auf FehlerBehoben geändert und es wird auf eine fallende Flanke reagiert
+      //
+      detachInterrupt(digitalPinToInterrupt(InterruptPin));
+      attachInterrupt(digitalPinToInterrupt(InterruptPin), FehlerBehoben, FALLING);
+      lcd.clear();
+      lcd.print("Fehler");
+      while(Starttaster==false)
+      {
+        digitalWrite(PB0,HIGH); // LED für die Visualisierung des Fehlers
+      }
+      digitalWrite(PB0,LOW);
+      lcd.clear();
+      lcd.print("Aktiv");
+      //
+      // Externer Interrupt wird konfiguriert: Interruptname (INT2) für PB2 wird gesucht, die ISR wird von 
+      // FehlerBehoben auf WakeUpHandler geändert und es wird auf eine fallende Flanke reagiert
+      //
+      detachInterrupt(digitalPinToInterrupt(InterruptPin));
+      attachInterrupt(digitalPinToInterrupt(InterruptPin), WakeUpHandler, FALLING);
+      Fehler=false;
+      Starttaster=false;
+      Zustand=2;
+      break;
+    }
   }
-
-  Serial.println("Gehe schlafen");
-  delay(100); // Wartezeit, um den Text auszugeben, bevor das Display ausgeschaltet wird
-  lcd.noBacklight();  // Hintergrundbeleuchtung wird ausgeschalten
-  lcd.noDisplay();  // Textanzeige wird ausgeschalten
-  LowPower.powerDown(SLEEP_FOREVER,ADC_OFF,BOD_OFF); // Power Down Mode wird aktiviert und erst bei externem Interrupt oder Reset wieder ausgeschalten
-  Serial.println("Bin aufgewacht");
-  lcd.backlight();
-  lcd.display();
-  lcd.clear();  // Text in Display löschen
-  lcd.print("Bin aufgewacht");
-  delay(1000);
 }
 
 //
 // ISR
 //
 void WakeUpHandler() {
+  Starttaster=true;
 }
+
+void FehlerBehoben()
+{
+  Starttaster=true;
+}
+
+
 
